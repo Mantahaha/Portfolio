a5a530e860968b664f8951e3a3ddd1d6ff5ecce8
Mantahaha <142211749+Mantahaha@users.noreply.github.com>
2023-08-21 20:36:52 +0200

    Treining 2

---
+++ Portfolio/src/Portfolio/AreaCircunferenciaPI.java
@@ -0,0 +1,24 @@
+package Portfolio;
+
+public class AreaCircunferenciaPI {
+
+	public static void main(String[] args) {
+		
+		double raio = 3.5;
+		double PI = 3.1415;
+		
+		double area = raio * PI * raio;
+		
+		System.out.println(area);
+	
+			
+		raio = 10;
+		area = raio * PI * raio;
+		
+		System.out.println(area);
+	
+	}
+	
+	
+	
+}
